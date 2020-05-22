5f4b705ae6b2e7bf26e4da8b9e5699feabae3b2a
Alejandro Maestro <amaestro8013@alumnos.uemc.es>
2020-05-22 14:35:30 +0200

    1/2 Gestion de canciones, hay que cambiar la BD

--- src/Controller/AdministradoresController.php
+++ src/Controller/AdministradoresController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Usuarios;
+use App\Entity\Autores;
+use App\Entity\Canciones;
+use App\Entity\Etiquetas;
 use App\Entity\Administradores;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -33,7 +36,7 @@ class AdministradoresController extends AbstractController
         ]);
     }
 
-        /**
+    /**
      * @Route("/gestion-usuarios", name="gestion-usuarios")
      */
     public function usuarios()
@@ -46,6 +49,131 @@ class AdministradoresController extends AbstractController
             'usuarios' => $usuarios,
         ]);
     }
+
+    /**
+     * @Route("/gestion-canciones", name="gestion-canciones")
+     */
+    public function canciones()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $canciones=$em->getRepository(Canciones::class)->findAll();
+
+        return $this->render('administradores/canciones.html.twig', [
+            'canciones' => $canciones,
+        ]);
+    }
+
+    /**
+     * @Route("/add-cancion", name="add-cancion")
+     */
+    public function addCancion(Request $datos)
+    {
+
+        if($_POST){
+
+            $em = $this->getDoctrine()->getManager(); 
+
+            $titulo = $datos->request->get('titulo');
+            $duracion = $datos->request->get('duracion');
+            $album = $datos->request->get('album');
+            $fecha = $datos->request->get('fecha');
+
+            $cancion= new Canciones();
+            $cancion->setTitulo($titulo);
+            $cancion->setDuracion($duracion);
+            $cancion->setAlbum($album);
+            $cancion->setFecha($fecha);
+
+            $autores=$datos->request->get('autor'); 
+            foreach ($autores as $autor){
+                if($cantante=$em->getRepository(Autores::class)->findOneBy(['nombre'=> $autor])){
+
+                    $cancion->addAutoresIdautore($cantante);
+                }
+                else{
+                    $newCantante= new Autores();
+                    $newCantante->setNombre($autor);
+
+                    $em->persist($newCantante);
+                    $em->flush();
+
+                    $cancion->addAutoresIdautore($newCantante);
+                }
+            }
+            
+            $etiquetas=$datos->request->get('etiqueta');
+            foreach ($etiquetas as $id){
+                
+                $etiqueta=$em->getRepository(Etiquetas::class)->find($id);
+                $cancion->addEtiquetasIdetiqueta($etiqueta);
+            }
+
+            $newEtiquetas=$datos->request->get('newEtiqueta');
+            foreach ($newEtiquetas as $newEtiqueta){
+                
+                $newTag= new Etiquetas();
+                $newTag->setNombre($newEtiqueta);
+
+                $em->persist($newTag);
+                $em->flush();
+
+                $cancion->addEtiquetasIdetiqueta($newTag);
+            }
+
+            $em->persist($cancion);
+            $em->flush();
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $etiquetas=$em->getRepository(Etiquetas::class)->findAll();
+
+
+        return $this->render('administradores/cancionAdd.html.twig',[
+            'etiquetas' => $etiquetas ,
+        ]);
+    }
+
+
+    /**
+     * @Route("/editar-cancion/{id}", name="editar-cancion")
+     */
+    public function editarCancion($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cancion=$em->getRepository(Canciones::class)->find($id);
+ 
+
+        $autores=$em->getRepository(Canciones::class)->findBy(['autoresIdautores'=>$id]);
+
+
+        // $query = $em->createQuery('SELECT Autores_idAutores FROM Autores');
+        // $autoresIds = $query->getResult();
+
+        var_dump($autoresIds);die;
+
+        return $this->render('administradores/cancionesEditar.html.twig',[
+            'cancion' => $cancion,
+        ]);
+    }
+
+
+    /**
+     * @Route("/eliminar-cancion/{id}", name="eliminar-cancion")
+     */
+    public function eliminarCancion($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $cancion=$em->getRepository(Canciones::class)->find($id);
+        
+        $em->remove($cancion);
+        $em->flush();
+        
+        return $this->redirectToRoute('gestion-canciones');
+    }
+
 
     /**
      * @Route("/eliminar-admin/{id}", name="eliminar-admin")
@@ -115,7 +243,7 @@ class AdministradoresController extends AbstractController
     /**
      * @Route("/edit-user", name="edit-user")
      */
-    public function esitUser(Request $datos)
+    public function editUser(Request $datos)
     {
         if($_POST){
             $em = $this->getDoctrine()->getManager();
