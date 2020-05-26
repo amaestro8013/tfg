f83c83083e6d080ce4f6008bfead0b0dbedf9538
Alejandro Maestro <amaestro8013@alumnos.uemc.es>
2020-05-26 04:44:40 +0200

    Pagina principal

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Etiquetas;
+use App\Entity\Perfiles;
+use App\Entity\Perfilesetiquetas;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +17,60 @@ class InicioController extends AbstractController
      */
     public function index()
     {
+
+        $em = $this->getDoctrine()->getManager();
+    
+        $etiquetas=$em->getRepository(Etiquetas::class)->findBy(['tipoautor'=>0]);
+        $autores=$em->getRepository(Etiquetas::class)->findBy(['tipoautor'=>1]);
+
         return $this->render('inicio/index.html.twig', [
-            'controller_name' => 'InicioController',
+            'etiquetas' => $etiquetas, 'autores' => $autores
+        ]);
+    }
+
+    /**
+     * @Route("/buscar", name="buscar")
+     */
+    public function buscar()
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        
+        if($_POST){
+
+            $etiquetas=$datos->request->get('etiqueta');
+            if($etiquetas){
+                
+                $perfil = new Perfiles();
+                $perfil->setNombre('NO REGISTRADO');
+                $em->persist($perfil);
+                $em->flush();
+                
+                foreach ($etiquetas as $id){
+                    
+                    $etiqueta=$em->getRepository(Etiquetas::class)->find($id);
+
+                    $PerfilEtiquetas = new Perfilesetiquetas();
+                    $PerfilEtiquetas->setEtiquetasIdetiquetas($etiqueta);
+                    $PerfilEtiquetas->setPerfilesIdperfiles($perfil);
+
+                    $em->persist($PerfilEtiquetas);
+                    $em->flush();
+                }
+            }else{
+                echo '<script>type="text/javascript">
+                    alert("Selecciona al menos un artista o genero musical");
+                </script>';
+            }
+        }
+
+
+        $this->busqueda($perfil) ;       
+
+        
+
+        return $this->render('inicio/busqueda.html.twig', [
+            'canciones' => $canciones,
         ]);
     }
 }
